5ad2ec09a6d8d23d6785c8b64fc3fe14487fab96
shrau94 <shrau94@gmail.com>
2020-03-17 17:12:29 +0000

    changes for publishing weather info

--- weather_info_cron/weather.py
+++ weather_info_cron/weather.py
@@ -2,43 +2,123 @@ import requests, json
 import time
 from datetime import datetime
 import csv
-  
-api_key = "2f30e9abf594e715af4449414b055800"
-base_url = "http://api.openweathermap.org/data/2.5/weather?"
-city_name = "Dublin,IE"
-weather_api_url = base_url + "appid=" + api_key + "&q=" + city_name + "&units=metric"
-response = requests.get(weather_api_url) 
-  
-x = response.json() 
+from awscrt import io, mqtt, auth, http
+from awsiot import mqtt_connection_builder
 
-  
-# Parsing the response to get weather attributes
-if x["cod"] != "404": 
+# Callback when connection is accidentally lost.
+def on_connection_interrupted(connection, error, **kwargs):
+    print("Connection interrupted. error: {}".format(error))
 
-    y = x["main"] 
+# Callback when an interrupted connection is re-established.
+def on_connection_resumed(connection, return_code, session_present, **kwargs):
+    print("Connection resumed. return_code: {} session_present: {}".format(return_code, session_present))
 
-    current_temperature = y["temp"] 
-  
-    current_pressure = y["pressure"] 
-  
-    current_humidiy = y["humidity"] 
-  
-    z = x["weather"] 
+    if return_code == mqtt.ConnectReturnCode.ACCEPTED and not session_present:
+        print("Session did not persist. Resubscribing to existing topics...")
+        resubscribe_future, _ = connection.resubscribe_existing_topics()
 
-    weather_description = z[0]["description"] 
+        # Cannot synchronously wait for resubscribe result because we're on the connection's event-loop thread,
+        # evaluate result with a callback instead.
+        resubscribe_future.add_done_callback(on_resubscribe_complete)
+
+def on_resubscribe_complete(resubscribe_future):
+    resubscribe_results = resubscribe_future.result()
+    print("Resubscribe results: {}".format(resubscribe_results))
+
+    for topic, qos in resubscribe_results['topics']:
+        if qos is None:
+            sys.exit("Server rejected resubscribe to topic: {}".format(topic))
+
+if __name__ == '__main__':
     
-    sun_times = x['sys']
+    endPt = "a37g54y6ddcht7-ats.iot.us-east-1.amazonaws.com"
+    clientId = "samples-client-id"
+    topic="weather/get"
     
-    sunrise = sun_times['sunrise']
-    sunset = sun_times['sunset']
-    
-    # Writing the data to a csv file
-    
-    with open('weather_data.csv', mode='w') as file:
-        writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        writer.writerow(['updated_time', 'temperature', 'sunrise', 'sunset', 'description'])
-        writer.writerow([datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), current_temperature, time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(sunrise)),
-                         time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(sunset)), weather_description])
+    api_key = "2f30e9abf594e715af4449414b055800"
+    base_url = "http://api.openweathermap.org/data/2.5/weather?"
+    city_name = "Dublin,IE"
+    weather_api_url = base_url + "appid=" + api_key + "&q=" + city_name + "&units=metric"
+    response = requests.get(weather_api_url) 
 
-else: 
-    print("Could not write data to csv") 
+    x = response.json() 
+    
+    event_loop_group = io.EventLoopGroup(1)
+    host_resolver = io.DefaultHostResolver(event_loop_group)
+    client_bootstrap = io.ClientBootstrap(event_loop_group, host_resolver)
+
+    mqtt_connection = mqtt_connection_builder.mtls_from_path(
+            endpoint=endPt,
+            cert_filepath="Certificates/cdc357b6af-certificate.pem.crt",
+            pri_key_filepath="Certificates/cdc357b6af-private.pem.key",
+            client_bootstrap=client_bootstrap,            
+            on_connection_interrupted=on_connection_interrupted,
+            on_connection_resumed=on_connection_resumed,
+            client_id=clientId,
+            clean_session=False,
+            keep_alive_secs=6)
+
+    print("Connecting to {} with client ID '{}'...".format(
+        endPt, clientId))
+    connect_future = mqtt_connection.connect()
+
+    # Future.result() waits until a result is available
+    connect_future.result()
+    print("Connected!")
+
+    # Parsing the response to get weather attributes
+    if x["cod"] != "404": 
+
+        y = x["main"] 
+
+        current_temperature = y["temp"] 
+        
+        max_temp = y['temp_max']
+        
+        min_temp = y['temp_min']
+        
+        feels_like = y['feels_like']
+
+        current_pressure = y["pressure"] 
+
+        current_humidiy = y["humidity"] 
+
+        z = x["weather"] 
+
+        weather_description = z[0]["description"] 
+
+        sun_times = x['sys']
+
+        sunrise = sun_times['sunrise']
+        sunset = sun_times['sunset']
+
+        # Writing the data to a csv file
+
+        with open('weather_data.csv', mode='w') as file:
+            writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+            writer.writerow(['updated_time', 'temperature', 'sunrise', 'sunset', 'description'])
+            writer.writerow([datetime.now().strftime("%m/%d/%Y, %H:%M:%S"), current_temperature, time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(sunrise)),
+                             time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(sunset)), weather_description])
+            
+        message = {
+            'sunrise': time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(sunrise)),
+            'sunset': time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(sunset)),
+            'temperature': current_temperature,
+            'max_temperature': max_temp,
+            'min_temperature': min_temp,
+            'weather_description': weather_description,
+            'feels_like': feels_like
+        }
+        print("Publishing message to topic '{}': {}".format(topic, message))
+        mqtt_connection.publish(
+            topic=topic,
+            payload=message,
+            qos=mqtt.QoS.AT_LEAST_ONCE)
+        # Disconnect
+        print("Disconnecting...")
+        disconnect_future = mqtt_connection.disconnect()
+        disconnect_future.result()
+        print("Disconnected!")
+
+    else: 
+        print("Could not get response from API") 
