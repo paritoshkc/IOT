76016a2b0f84e65582a9812862883e504061bb13
shuchi03 <shuchitakapoor99@gmail.com>
2020-03-18 01:58:38 +0000

    Added light and blind switch with digital clock

---
+++ Android/SmartHomeSystem/app/src/main/java/com/example/smarthomesystem/ui/main/OutOfHome.java
@@ -0,0 +1,22 @@
+package com.example.smarthomesystem.ui.main;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.smarthomesystem.R;
+
+public class OutOfHome extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_out_of_home, container, false);
+    }
+}
